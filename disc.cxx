ded24c32d1ad7b3174d4e4d33451ce57b8375b70
Robert Ian Hynes <robert.hynes.astro@gmail.com>
2025-08-25 09:06:35 -0500

    Update disc.cxx
    Removed version and date

--- disc.cxx
+++ disc.cxx
@@ -1,9 +1,6 @@
 /*
   Class to calculate properties of an accretion disc
 
-  Released as part of BinSim 1.0
-  This release built on August 25 2025
-
   Author: Robert I. Hynes (rhynes@lsu.edu)
           Louisiana State University
           Department of Physics and Astronomy
